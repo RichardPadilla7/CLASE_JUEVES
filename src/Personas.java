6cf2e0a2613ce3a007f7ee93d08cf6b5f71fb9c2
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-10 09:48:43 -0500

    Richard commit!

--- src/Personas.java
+++ src/Personas.java
@@ -1,13 +1,35 @@
-public class Vehiculos {
+public class Personas{
+    //Atributos
+    String nombre, direccion, ocupacion;
+    int edad, telefono;
+
+    //Constructor
+    public Personas(String nombre, String direccion,String ocupacion, int edad, int telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.ocupacion = ocupacion;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    //Funcion
+    public void presentarse(){
+        System.out.println("Hola!, Mi nombre es " + nombre + " y vivo en " + direccion + ". Soy " + ocupacion +
+                " con edad de " + edad + ". Contactame a este numero " + telefono);
+    }
+
+
+
     public static void main(String[] args) {
 
-        String color;
-        String marca;
-        int n_llantas;
-        double precio;
-        double tamanio;
+        Personas estudiante1 = new Personas("Richard","Zabala","Estudiante",20,979169393);
+        estudiante1.presentarse();
 
-        
+        Personas Docente1 = new Personas("Yadira","Solca","Docente",25,321654987);
+        Docente1.presentarse();
+
+        Personas Secretario1 = new Personas("Melany","Zabala","Secretaria",20,987654321);
+        Secretario1.presentarse();
 
     }
 }
