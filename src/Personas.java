159024db6f2d508ca4bfcbde06dd6b8790ea5d63
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-10 09:48:22 -0500

    Richard

---
+++ src/Personas.java
@@ -0,0 +1,13 @@
+public class Vehiculos {
+    public static void main(String[] args) {
+
+        String color;
+        String marca;
+        int n_llantas;
+        double precio;
+        double tamanio;
+
+        
+
+    }
+}
