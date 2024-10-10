99f2a6972b1c4dc5d98350f657c16e13f550d55e
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-10 09:54:13 -0500

    commit 2

--- src/Personas.java
+++ src/Personas.java
@@ -18,10 +18,14 @@ public class Personas{
                 " con edad de " + edad + ". Contactame a este numero " + telefono);
     }
 
+    public void correr(){
+        System.out.println("Con extio!!. El corredor "+ nombre + " del sector " + direccion + " con edad "+ edad + ". A ganado la medalla de mejor recorido en la maraton. Contactos: " + telefono);
+    }
+
 
 
     public static void main(String[] args) {
-
+        /*
         Personas estudiante1 = new Personas("Richard","Zabala","Estudiante",20,979169393);
         estudiante1.presentarse();
 
@@ -30,6 +34,21 @@ public class Personas{
 
         Personas Secretario1 = new Personas("Melany","Zabala","Secretaria",20,987654321);
         Secretario1.presentarse();
+        */
+
+        Personas corredor1 = new Personas("Melany", "Zabala", "Secretaria", 20, 321654987);
+        corredor1.correr();
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
